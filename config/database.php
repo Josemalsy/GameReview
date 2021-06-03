168ed6698d3655ad65ee1ac2964c771b325c4cf3
José Manuel <josema240@hotmail.com>
2021-06-03 17:35:57 +0200

    Revert "no message"
    This reverts commit 057bb66921f91b51d45b440bde307e0486e1ceb4.

--- config/database.php
+++ config/database.php
@@ -2,15 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysql($server, $username, $password, $db);
-
 return [
 
     /*
