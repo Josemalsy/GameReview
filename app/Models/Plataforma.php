bcf110ee8551184bdad73c3a960b852505c64ee2
José Manuel <josema240@hotmail.com>
2021-06-03 21:30:22 +0200

    no message

---
+++ app/Models/Plataforma.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plataforma extends Model
+{
+    use HasFactory;
+
+  protected $fillable = [
+    'nombre',
+    'fabricante',
+	];
+
+
+  public function games(){
+		return $this->belongsToMany(Game::class)->orderBy('id','desc');
+  }
+
+}
