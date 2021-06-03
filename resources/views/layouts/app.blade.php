64d0209c6cd9f4536ae4e7b5a968df7c2d3e6e41
José Manuel <josema240@hotmail.com>
2021-06-03 13:14:18 +0200

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 	<nav id="header" class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
     <div class="container">
       <a class="navbar-brand" href="{{ url('/') }}">
-        <img src='{{asset("storage/logos/logo.png")}}'>
+        <img src='{{asset("storage/logos/Logo.png")}}'>
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
