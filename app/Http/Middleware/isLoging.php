2f64930723abe17b277ec7f1db2f46f59dd1d616
José Manuel <josema240@hotmail.com>
2021-06-03 12:26:01 +0200

    no message

--- app/Http/Middleware/isLoging.php
+++ app/Http/Middleware/isLoging.php
@@ -3,20 +3,13 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class isLoging
+class IsLoging
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
+  public function handle($request, Closure $next)
     {
+
       if(!Auth::guest()){
         $user = Auth::user()->estado;
         if($user == 'Expulsado'){
