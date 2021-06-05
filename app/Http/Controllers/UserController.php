960214d3c2397984da0261e4a75c2a81294415c7
José Manuel <josema240@hotmail.com>
2021-06-05 10:39:37 +0200

    no message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,8 @@ class UserController extends Controller
 
   public function getAllUsers(Request $request){
 
+    dd($request->all());
+
 		$buscador = $request['params']['filters'];
     $orden = $request['params']['orden'];
 
