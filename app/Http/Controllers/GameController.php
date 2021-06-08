f03ad6ff7ba4e17ad5e3fa50f0b3ac8eff58464d
José Manuel <josema240@hotmail.com>
2021-06-08 17:59:37 +0200

    no message

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,14 +10,11 @@ use App\Models\Game_Genero;
 use App\Models\Game_User;
 use App\Models\Review;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
 
   public function index(Request $request) {
-
-    dd(auth()->user()->id);
 
 		$buscador = $request->get('filters');
     $orden = $request->get('orden');
